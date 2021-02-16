a3b76c68514e8bd9ef5803e5a072f545161ec57f
Alex Hauka <alexhauka@gmail.com>
2021-02-16 08:56:23 -0800

    add appointment parent component and two child components with stories

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styles.scss";
+
+import Header from "./Header";
+
+
+export default function Appointment(props) {
+  
+  
+  return(
+    <article className="appointment">
+
+    </article>
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,3 +137,13 @@ storiesOf("InterviewerList", module)
     />
 ));
 
+
+
+storiesOf("Appointment", module)
+.addParameters({
+  backgrounds: [{name: "white", value: "#fff", default: true}]
+})
+.add("Appointment", () => <Appointment />)
+.add("Appointment with Time", () => <Appointment time="12pm" />)
+.add("Header", () => <Header time="12pm" />)
+.add("Empty", () => <Empty  onAdd={action("onAdd")}/>)
